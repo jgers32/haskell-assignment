66af0f79b7105af54bb0652fe640a9e023e911ff
Julia Gersey <jgersey20@bw.edu>
2023-10-18 12:40:49 -0400

    main and option select partially added

--- todo.hs
+++ todo.hs
@@ -1,1 +1,23 @@
 -- this is the file for our assignment
+
+
+-- goal 1: get the option the user inputs (add, print, search)
+optionSelect option
+  | option == "add" = "then add"
+  | otherwise = "other"
+
+  
+
+main = do
+  putStrLn "Enter option:"
+  input <- getLine
+  let option = read input :: String
+
+  let x = optionSelect option
+  putStrLn $ "option:" ++ x
+
+
+
+-- printTaskList (x:xs) = do
+--   putStrLn & "Here are your tasks:" ++ x
+--   printTaskList xs
