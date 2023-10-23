32f78d0ebceb2cbd3385a313c3890d40dce05ebd
Julia Gersey <jgersey20@bw.edu>
2023-10-23 14:55:58 -0400

    everything done until search

--- todo.hs
+++ todo.hs
@@ -1,40 +1,39 @@
 -- Julia Gersey & Hannah Ogden
 
-addTask task list = do
-  let updatedList = list ++ [task]
-  printTaskList updatedList
+addTask task list = list ++ [task]
 
 printTaskList [] = do
-  putStrLn "End"
+  putStrLn ""
 
 printTaskList (x:xs) = do
-  putStrLn $ " Task: " ++ x
+  putStrLn $ "\t" ++ x
   printTaskList xs
 
-searchTaskList task list = do
-  -- build this part out 
+-- searchTaskList task (x:xs) = do
+--   if task == x then putStrLn $ "Found: " ++ x 
+--   else  x == xs "Could not find"
  
 buildTaskList list = do
-  --addTask "gersey" list           addTask works (hard coded)
-  --printTaskList list              printTaskList works (hard coded)
-  -- build & implement searchTaskList
-
-  putStrLn "Enter option: "
+  putStrLn "Below are the options: \n\tadd\n\tprint\n\tsearch\nEnter option: "
   option <- getLine
 
   if option == "add" then do
     putStrLn "Enter task to add: "
     task <- getLine
-    addTask task list
+    let updatedList = addTask task list
+    buildTaskList updatedList
   else if option == "print" then do
+    putStrLn $ "Here are your tasks: " 
     printTaskList list
-  else if option == "search" then do
-    searchTaskList task list
-  else putStrLn "error"
+  -- else if option == "search" then do
+  --   putStrLn "Enter task to search: "
+  --   task <- getLine
+  --   let searchResult = searchTaskList task list
+  --   putStrLn $ searchResult
+  else putStrLn "Error"
 
-  buildTaskList updatedList
+  buildTaskList list
 
 main :: IO ()
 main = do
-  putStrLn "Below are the options: \n\tadd\n\tprint\n\tsearch"
   buildTaskList []
