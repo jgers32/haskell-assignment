37e2a7fff1bf86546b524905207d82cc4a4ccebc
Julia Gersey <jgersey20@bw.edu>
2023-10-24 15:28:53 -0400

    completed assignment

--- todo.hs
+++ todo.hs
@@ -9,31 +9,37 @@ printTaskList (x:xs) = do
   putStrLn $ "\t" ++ x
   printTaskList xs
 
--- searchTaskList task (x:xs) = do
---   if task == x then putStrLn $ "Found: " ++ x 
---   else  x == xs "Could not find"
+searchTaskList task [] = False
+
+searchTaskList task (x:xs) =
+  if task == x
+    then True
+    else searchTaskList task xs
  
 buildTaskList list = do
   putStrLn "Below are the options: \n\tadd\n\tprint\n\tsearch\nEnter option: "
   option <- getLine
 
   if option == "add" then do
-    putStrLn "Enter task to add: "
+    putStrLn "Enter Task to Add: "
     task <- getLine
     let updatedList = addTask task list
     buildTaskList updatedList
   else if option == "print" then do
     putStrLn $ "Here are your tasks: " 
     printTaskList list
-  -- else if option == "search" then do
-  --   putStrLn "Enter task to search: "
-  --   task <- getLine
-  --   let searchResult = searchTaskList task list
-  --   putStrLn $ searchResult
+  else if option == "search" then do
+    putStrLn "Enter Task to Search: "
+    task <- getLine
+    let found = searchTaskList task list
+    if found
+      then putStrLn $ "Found: " ++ task
+      else putStrLn $ "Could not find: " ++ task
   else putStrLn "Error"
 
   buildTaskList list
 
 main :: IO ()
 main = do
+  putStrLn $ "Welcome to Task Manager 3000"
   buildTaskList []
